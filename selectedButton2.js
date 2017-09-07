879e426d9a76bf1e862244daf47ba2723fa156d8
Salilthip Phuklang <ung_1002@hotmail.com>
2017-09-07 15:31:25 +0700

    Make the pointer move by hand direction

--- selectedButton2.js
+++ selectedButton2.js
@@ -16,45 +16,61 @@ myState.preload = function(){
 myState.create = function(){
   Kiwi.State.prototype.create.call( this );
   this.background = new Kiwi.GameObjects.StaticImage(this, this.textures.background, 0, 0);
-  this.character = new Pointer( this, this.textures.characterSprite, 350, 100 );
+  this.character = new Pointer( this, this.textures.characterSprite, 900, 500 );
   this.character.physics = this.components.add(new Kiwi.Components.ArcadePhysics(this.character, this.character.box));
-  this.nuclear = new Button( this, this.textures.nuclear,300,150);
-  this.water = new Button( this, this.textures.water, 600,150);
-	this.wind = new Button( this, this.textures.wind, 900,150);
-  this.coal = new Button( this, this.textures.coal, 1200,150);
-  this.sun = new Button( this, this.textures.sun, 300,473);
-  this.underworld = new Button( this, this.textures.underworld, 600,473);
-  this.petroleum = new Button( this, this.textures.petroleum, 900,473);
-	this.fire = new Button( this, this.textures.fire, 1200,473);
+  this.nuclear = new Button( this, this.textures.nuclear,298,150);
+  this.water = new Button( this, this.textures.water, 656,150);
+	this.wind = new Button( this, this.textures.wind, 1014,150);
+  this.coal = new Button( this, this.textures.coal, 1372,150);
+  this.sun = new Button( this, this.textures.sun, 298,473);
+  this.underworld = new Button( this, this.textures.underworld, 656,473);
+  this.petroleum = new Button( this, this.textures.petroleum, 1014,473);
+	this.fire = new Button( this, this.textures.fire, 1372,473);
 
   // this.belt = new Belt(this,this.textures.belt, 0,800);
 
 
   this.addChild(this.background);
   // this.addChild(this.belt);
+	this.addChild(this.sun);
+	this.addChild(this.underworld);
+	this.addChild(this.petroleum);
+	this.addChild(this.fire);
   this.addChild( this.character );
   this.addChild(this.nuclear);
   this.addChild(this.water);
 	this.addChild(this.wind);
   this.addChild(this.coal);
-	this.addChild(this.sun);
-  this.addChild(this.underworld);
-  this.addChild(this.petroleum);
-	this.addChild(this.fire);
   this.control = Kiwi.Plugins.LEAPController.createController();
 }
 
 myState.update = function(){
   Kiwi.State.prototype.update.call(this);
-      this.character.x = (this.control.hands[0].pointables[0].tipX* 1.7) + 400;
-      this.character.y =((-1 * this.control.hands[0].pointables[0].tipY)*1.7) + 600;
-
 
       if(this.control.hands[0].pointables[0].touchZone == "none"){
         console.log('none');
         this.character.animation.play('point');
 
     }else if(this.control.hands[0].pointables[0].touchZone == "hovering"){
+
+			let xVal = this.control.hands[0].pointables[0].tipX;
+			let yVal = (this.control.hands[0].pointables[0].tipY);
+
+				//กำหนดให้ LEAP รู้ว่าเอามือลงโดยการอ้างอิงจากแกนYที่ตำแหน่ง 200
+				if(yVal <= 200) yVal = (200 - yVal);
+				else yVal = -1*(yVal -200);
+
+				//ดักขอบ
+				if(this.character.x + xVal*0.05 <0 ) this.character.x = (-1*(this.character.x + xVal*0.05));
+				else if(xVal > 1920 - this.character.x){
+					this.character.x = 1920 - xVal;
+				} else this.character.x += xVal*0.05;
+				if(this.character.y + yVal*0.019 <0 ) this.character.y = (-1*(this.character.y + yVal*0.019));
+				else if(yVal > 900- this.character.y){
+					this.character.y = 900 - yVal;
+				} else this.character.y += yVal*0.019;
+
+
         if(this.character.physics.overlaps(this.nuclear)){
             console.log("Change color");
             this.nuclear.animation.play('float');
@@ -62,16 +78,35 @@ myState.update = function(){
             this.water.animation.play('float');
         }else if (this.character.physics.overlaps(this.coal)) {
             this.coal.animation.play('float');
+        }else if (this.character.physics.overlaps(this.fire)) {
+            this.fire.animation.play('float');
+        }else if (this.character.physics.overlaps(this.petroleum)) {
+            this.petroleum.animation.play('float');
+        }else if (this.character.physics.overlaps(this.sun)) {
+            this.sun.animation.play('float');
+        }else if (this.character.physics.overlaps(this.wind)) {
+            this.wind.animation.play('float');
+        }else if (this.character.physics.overlaps(this.underworld)) {
+            this.underworld.animation.play('float');
         }
         console.log('hovering');
     } else if(this.control.hands[0].pointables[0].touchZone == "touching"){
         if(this.character.physics.overlaps(this.nuclear)){
-            console.log("Change color");
             this.nuclear.physics.velocity.y = 70;
         }else if (this.character.physics.overlaps(this.water)) {
             this.water.physics.velocity.y = 70;
         }else if (this.character.physics.overlaps(this.coal)) {
-            this.water.physics.velocity.y = 70;
+            this.coal.physics.velocity.y = 70;
+        }else if (this.character.physics.overlaps(this.fire)) {
+            this.fire.physics.velocity.y = 70;
+        }else if (this.character.physics.overlaps(this.petroleum)) {
+            this.petroleum.physics.velocity.y = 70;
+        }else if (this.character.physics.overlaps(this.sun)) {
+            this.sun.physics.velocity.y = 70;
+        }else if (this.character.physics.overlaps(this.wind)) {
+            this.wind.physics.velocity.y = 70;
+        }else if (this.character.physics.overlaps(this.underworld)) {
+            this.underworld.physics.velocity.y = 70;
         }
         this.character.animation.play('press');
 
@@ -98,7 +133,7 @@ var Button = function (state,image, x, y){
 
 		this.fallen = function(){
 			this.animation.play('lay');
-			if(this.y >= 680 ){
+			if(this.y >= 600 ){
 				console.log(this.y);
 				this.physics.velocity.y = 0;
 				this.x += 4.07;
@@ -164,14 +199,14 @@ loadingState.preload = function(){
 ///////////////////////////////////////
 	//Environment Assets
 	this.addSpriteSheet('characterSprite','pointer.png',125,75);
-	this.addSpriteSheet('underworld','button/btn1_underworld.png',500,300);
-	this.addSpriteSheet('nuclear','button/btn2_nuclear.png',500,300);
-	this.addSpriteSheet('wind','button/btn3_wind.png',500,300);
-	this.addSpriteSheet('sun','button/btn4_sun.png',500,300);
-	this.addSpriteSheet('water','button/btn5_water.png',500,300);
-	this.addSpriteSheet('fire','button/btn6_fire.png',500,300);
-	this.addSpriteSheet('coal','button/btn7_coal.png',500,300);
-	this.addSpriteSheet('petroleum','button/btn8_petroleum.png',500,300);
+	this.addSpriteSheet('underworld','button/btn1_underworld.png',250,300);
+	this.addSpriteSheet('nuclear','button/btn2_nuclear.png',250,300);
+	this.addSpriteSheet('wind','button/btn3_wind.png',250,300);
+	this.addSpriteSheet('sun','button/btn4_sun.png',250,300);
+	this.addSpriteSheet('water','button/btn5_water.png',250,300);
+	this.addSpriteSheet('fire','button/btn6_fire.png',250,300);
+	this.addSpriteSheet('coal','button/btn7_coal.png',250,300);
+	this.addSpriteSheet('petroleum','button/btn8_petroleum.png',250,300);
 	this.addSpriteSheet('belt','test-belt.png',1920,20);
 	this.addImage('background','background.png');
 
