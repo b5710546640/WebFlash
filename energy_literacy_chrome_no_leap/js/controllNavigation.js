f6d0645b86d99c95d779e8d0549e2df8b218f1f6
Salilthip Phuklang <ung_1002@hotmail.com>
2017-10-03 10:47:48 +0700

    Update hand swipe

--- energy_literacy_chrome_no_leap/js/controllNavigation.js
+++ energy_literacy_chrome_no_leap/js/controllNavigation.js
@@ -59,23 +59,6 @@ if (external.AddListenerForLeapMotion)
 	external.AddListenerForLeapMotion(LeapHandler);
 
 function LeapHandler(fh) {
-	//$("#leap-info pre").text("fingers: " + JSON.stringify(fh.fingers, null, " "));
-	//$("#leap-info pre").text("gesture: " + JSON.stringify(fh.gesture, null, " "));
-	// $("#leap-info pre").text("hands: " + JSON.stringify(fh.hands, null, " "));
-	//
-	// LeapPointerDraw(fh.hands);
-
-	// if (leapEnable) {
-		console.log(currentPage);
-	//
-	// 	//check fingers appear
-	// 	if (fh.fingers.length>0) {
-	// 		if (typeof curPage.noTimeout !== "undefined") {
-	// 			//PageTransitions.nextPage({"animation": 58, "showPage": curPage.linkPages.right});
-	// 			PageTransitions.nextPage({"animation": 1, "showPage": curPage.linkPages.right});
-	// 			return;
-	// 		}
-	// 	}
 
 		LeapHandChangePage(fh.hands);
 		//LeapGestureChangePage(fh.gesture, curPage);
@@ -94,18 +77,18 @@ function LeapHandler(fh) {
 		 					if (velX>0) {
 		 						console.log('LEFT'+velX);
 		            // if(previousPage!='none')
-		            // window.location.href = previousPage;
+		            window.location.href = previousPage;
 
 		 					} else {
 		 					console.log('RIGHT');
-		            // window.location.href = nextPage;
+		            window.location.href = nextPage;
 
 		 					}
 		 				}
 		 			}  else {
  				if (Math.abs(velY)>500) {
  					if (velY>0) {
- 					window.location.href = '03-5_Factory.html';
+ 				// 	window.location.href = '03-5_Factory.html';
  					} else {
  					// 	window.location.href = '../../06_Lamp.html';
  					}
